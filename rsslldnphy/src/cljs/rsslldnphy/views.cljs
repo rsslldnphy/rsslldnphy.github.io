03d751d394e9dc3c9305f2fa75fcdfbf7653719b
Russell Dunphy <russell@russelldunphy.com>
2018-08-25 15:24:05 +0100

    target=_blank on social links

--- rsslldnphy/src/cljs/rsslldnphy/views.cljs
+++ rsslldnphy/src/cljs/rsslldnphy/views.cljs
@@ -9,19 +9,23 @@
     [rsslldnphy.songs.views :as songs]
     [rsslldnphy.words.views :as words]))
 
+(defn layout
+  [current-page & forms]
+  (into [:div {:style {:margin-top "15px" :font-family "Quicksand, sans-serif"}}
+         (when-not (= :home current-page)
+           [:div.col-md-12 {:style {:pointer-events "none" :overflow "hidden"}}
+            [:strong (name current-page)]
+            [:br]
+            [:br]])]
+        forms))
+
 (defn main-panel []
   []
   (let [current-page (re-frame/subscribe [:current-page])]
     (fn []
-      [:div {:style {:margin-top "15px" :font-family "Quicksand, sans-serif"}}
-       (when-not (= :home @current-page)
-         [:div.col-md-12 {:style {:pointer-events "none" :overflow "hidden"}}
-          [:strong (name @current-page)]
-          [:br]
-          [:br]])
-       (case @current-page
-         :home     [home/home]
-         :songs    [songs/songs]
-         :pictures [pictures/pictures]
-         :words    [words/words]
-         [:div])])))
+      (case @current-page
+        :home     [layout :home     [home/home]]
+        :songs    [layout :songs    [songs/songs]]
+        :pictures [layout :pictures [pictures/pictures]]
+        :words    [layout :words    [words/words]]
+        [:div]))))
