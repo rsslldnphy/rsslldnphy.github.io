0152577f63a52e2c4c171b66b89424787b57e90d
Russell Dunphy <russell@russelldunphy.com>
2021-12-16 11:52:29 +0000

    First pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as UI from "@mui/material";
+import images, { image37 } from "./images";
+import _ from "lodash";
 
 function App() {
+  const theme = UI.useTheme();
+  const sm = UI.useMediaQuery(theme.breakpoints.up("sm"));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UI.Box sx={{ overflow: "hidden" }}>
+      <img
+        src={image37}
+        alt="Christmas cartoon"
+        style={{ width: "calc(100% - 16px)", margin: "8px 8px 0 8px" }}
+      />
+      <UI.ImageList
+        variant="masonry"
+        cols={sm ? 2 : 1}
+        gap={8}
+        sx={{ mx: 1, mt: 0, mb: 1 }}
+      >
+        {_.reverse(images).map((image, index) => (
+          <UI.ImageListItem key={index}>
+            <img src={image} loading="lazy" />
+          </UI.ImageListItem>
+        ))}
+      </UI.ImageList>
+    </UI.Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
+import * as theme from "./theme";
+import { Global } from "@emotion/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme.theme}>
+      <CssBaseline />
+      <Global styles={theme.global} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
