944e09d461a13988064d9205191054f309bbed3b
Russell Dunphy <russell@russelldunphy.com>
2025-01-24 16:04:11 +0000

    animation

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,6 @@
+import "@fontsource/poppins";
+import "@fontsource/manrope";
+
 import {
   isRouteErrorResponse,
   Links,
@@ -33,7 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-screen">
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import type { Route } from "./+types/home";
 import me from "~/images/me.jpg";
 
@@ -9,24 +10,65 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const [on, setOn] = useState(true);
+  const toggle = useCallback(() => setOn((prev) => !prev), []);
+
   return (
-    <div>
-      <div className="bg-pink-500 p-4">
+    <div className="flex flex-col-reverse items-stretch h-full z-10">
+      <div className="bg-emerald-400 flex-grow flex z-0">
+        <div
+          className={`w-full flex align-center justify-center relative transition-all ease-in-out duration-500 ${
+            on ? "p-8 " : "p-2 delay-1000"
+          }`}
+        >
+          <h1
+            style={{
+              // lineHeight: "32px",
+              marginBlockStart: "0",
+              marginBlockEnd: "0",
+            }}
+            className={`w-full ${
+              on ? "text-5xl" : "text-2xl delay-1000"
+            } text-white font-semibold flex transition-all ease-in-out duration-500`}
+            onClick={toggle}
+          >
+            <div className="grow text-right" style={{ lineHeight: "32px" }}>
+              Russell
+            </div>
+            <div
+              style={{ lineHeight: "32px" }}
+              className={
+                on
+                  ? "w-4 transition-all ease-in-out duration-1000 delay-500"
+                  : "w-32 transition-all ease-in-out duration-1000"
+              }
+            />
+            <div className="grow text-left" style={{ lineHeight: "32px" }}>
+              Dunphy
+            </div>
+          </h1>
+        </div>
+      </div>
+      <div
+        className={
+          on
+            ? "z-100 bg-pink-500 h-1/2 flex items-center transition-all ease-in-out duration-1000 p-2 delay-500"
+            : "z-100 bg-pink-500 h-24 flex items-start transition-all ease-in-out duration-1000 delay-500 p-2"
+        }
+      >
         <div
           style={{
             backgroundImage: `url(${me})`,
             backgroundSize: "100% auto",
             backgroundRepeat: "no-repeat",
+            zIndex: "1000",
           }}
-          className="rounded-full size-72 border-8 border-white mx-auto"
+          className={
+            on
+              ? "rounded-full size-72 border-8 border-white my-auto transition-all ease-in-out duration-1000 delay-1000 mx-auto"
+              : "rounded-full size-32 border-8 border-white my-auto transition-all ease-in-out duration-1000 mx-auto"
+          }
         />
-      </div>
-      <div className="bg-emerald-400 p-4">
-        <div className="w-full flex align-center justify-center">
-          <h1 className="mx-auto text-3xl text-white uppercase font-semibold">
-            Russell Dunphy
-          </h1>
-        </div>
       </div>
     </div>
   );
